d41a315df743042d3bbd3f2359cb592e5c83c95c
Vijaykoli-09 <vijay.koli@ashdipitsolutions.in>
2026-03-04 10:46:42 +0530

    Update AuthController.java
    added health point for uptimebot testing

--- backend/src/main/java/com/garment/controller/AuthController.java
+++ backend/src/main/java/com/garment/controller/AuthController.java
@@ -61,35 +61,10 @@ public class AuthController {
 
         return ResponseEntity.ok(userService.login(req));
     }
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
 }
-//// src/main/java/com/garment/controller/AuthController.java
-//package com.garment.controller;
-//
-//
-//import com.garment.DTO.LoginRequest;
-//import com.garment.DTO.LoginResponse;
-//import com.garment.DTO.SignupRequest;
-//import com.garment.service.UserService;
-//import jakarta.validation.Valid;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/auth")
-//@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
-//public class AuthController {
-//
-//    private final UserService userService;
-//    public AuthController(UserService userService) { this.userService = userService; }
-//
-//    @PostMapping("/signup")
-//    public ResponseEntity<?> signup(@Valid @RequestBody SignupRequest req) {
-//        userService.signup(req);
-//        return ResponseEntity.ok("User registered successfully");
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest req) {
-//        return ResponseEntity.ok(userService.login(req));
-//    }
-//}
+
+
